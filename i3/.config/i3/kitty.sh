8289f0d52c6fda3ce707db5b921efe7f3273177c
Maksym Tsapiv <maksym.tsapiv@ucu.edu.ua>
2023-09-21 14:59:33 +0300

    Add some dotfiles

---
+++ i3/.config/i3/kitty.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+filter="(zsh|bash|vi|mc)"
+winpid=$(xprop -id $(xdotool getactivewindow) _NET_WM_PID|awk '{print $3}')
+if [ -n "$winpid" ]; then 
+    lpid=$(pstree -aApT ${winpid}|grep -E  "\\-${filter},[0-9]+" -o|tail -n1|awk -F, '{print $2}')
+    if [ -n "${lpid}" ]; then
+        path=$(readlink /proc/${lpid}/cwd)
+        # echo $path
+    fi
+fi
+kitty $path &
